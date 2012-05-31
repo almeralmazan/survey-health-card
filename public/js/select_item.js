d20cfd2af6c826fb6f282e968cbb2bb102daf1b1
Almer Almazan <almeralmazan@gmail.com>
2012-05-31 15:41:48 +0800

    added json for regions and provinces

---
+++ public/js/select_item.js
@@ -0,0 +1,149 @@
+var regiondb = new Object();
+	
+regiondb["Region I"] = [{value:"Ilocos Norte", text:"Ilocos Norte"},
+                      {value:"Ilocos Sur", text:"Ilocos Sur"},
+                      {value:"La Union", text:"La Union"},
+                      {value:"Pangasinan", text:"Pangasinan"},
+                      {value:"Dagupan City", text:"Dagupan City"}];
+
+regiondb["Region II"] = [{value:"Batanes", text:"Batanes"},
+                      {value:"Cagayan", text:"Cagayan"},
+                      {value:"Isabela", text:"Isabela"},
+                      {value:"Santiago City", text:"Santiago City"},
+                      {value:"Quirino", text:"Quirino"},
+                      {value:"Nueva Vizcaya", text:"Nueva Vizcaya"}];
+
+regiondb["Region III"] = [{value:"Aurora", text:"Aurora"},
+                      {value:"Bataan", text:"Bataan"},
+                      {value:"Bulacan", text:"Bulacan"},
+                      {value:"Nueva Ecija", text:"Nueva Ecija"},
+                      {value:"Pampanga", text:"Pampanga"},
+                      {value:"Tarlac", text:"Tarlac"},
+                      {value:"Zambales", text:"Zambales"},
+                      {value:"Angeles City", text:"Angeles City"}];
+
+regiondb["Region IVa"] = [{value:"Batangas", text:"Batangas"},
+                      {value:"Cavite", text:"Cavite"},
+                      {value:"Laguna", text:"Laguna"},
+                      {value:"Quezon", text:"Quezon"},
+                      {value:"Rizal", text:"Rizal"},
+                      {value:"Antipolo City", text:"Antipolo City"},
+                      {value:"Lucena City", text:"Lucena City"}];
+
+regiondb["Region IVb"] = [{value:"Marinduque", text:"Marinduque"},
+                      {value:"Occidental Mindoro", text:"Occidental Mindoro"},
+                      {value:"Oriental Mindoro", text:"Oriental Mindoro"},
+                      {value:"Palawan", text:"Palawan"},
+                      {value:"Romblon", text:"Romblon"}];
+
+regiondb["Region V"] = [{value:"Albay", text:"Albay"},
+                      {value:"Camarines Norte", text:"Camarines Norte"},
+                      {value:"Camarines Sur", text:"Camarines Sur"},
+                      {value:"Catanduanes", text:"Catanduanes"},
+                      {value:"Masbate", text:"Masbate"},
+                      {value:"Sorsogon", text:"Sorsogon"}];
+
+regiondb["Region VI"] = [{value:"Aklan", text:"Aklan"},
+                      {value:"Antique", text:"Antique"},
+                      {value:"Capiz", text:"Capiz"},
+                      {value:"Guimaras", text:"Guimaras"},
+                      {value:"Iloilo", text:"Iloilo"},
+                      {value:"Bacolod City", text:"Bacolod City"},
+                      {value:"Palawan", text:"Palawan"}];
+
+regiondb["Region VII"] = [{value:"Bohol", text:"Bohol"},
+                      {value:"Cebu", text:"Cebu"},
+                      {value:"Negros Oriental", text:"Negros Oriental"},
+                      {value:"Siquijor", text:"Siquijor"}];
+
+regiondb["Region VIII"] = [{value:"Biliran", text:"Biliran"},
+                      {value:"Eastern Samar", text:"Eastern Samar"},
+                      {value:"Leyte", text:"Leyte"},
+                      {value:"Northern Samar", text:"Northern Samar"},
+                      {value:"Ormoc City", text:"Ormoc City"},
+                      {value:"Samar", text:"Samar"},
+                      {value:"Southern Leyte", text:"Southern Leyte"},
+                      {value:"Tacloban City", text:"Tacloban City"}];
+
+regiondb["Region IX"] = [{value:"Isabela City", text:"Isabela City"},
+                      {value:"Zamboanga City", text:"Zamboanga City"},
+                      {value:"Zamboanga Del Norte", text:"Zamboanga Del Norte"},
+                      {value:"Zamboanga Del Sur", text:"Zamboanga Del Sur"},
+                      {value:"Zamboanga Sibugay", text:"Zamboanga Sibugay"}];
+
+regiondb["Region X"] = [{value:"Bukidnon", text:"Bukidnon"},
+                      {value:"Cagayan De Oro City", text:"Cagayan De Oro City"},
+                      {value:"Camiguin", text:"Camiguin"},
+                      {value:"Iligan City", text:"Iligan City"},
+                      {value:"Lanao Del Norte", text:"Lanao Del Norte"},
+                      {value:"Misamis Occidental", text:"Misamis Occidental"},
+                      {value:"Misamis Oriental", text:"Misamis Oriental"}];
+
+regiondb["Region XI"] = [{value:"Compostela Valley", text:"Compostela Valley"},
+                      {value:"Davao City", text:"Davao City"},
+                      {value:"Davao Del Norte", text:"Davao Del Norte"},
+                      {value:"Davao Del Sur", text:"Davao Del Sur"},
+                      {value:"Davao Oriental", text:"Davao Oriental"}];
+
+regiondb["Region XII"] = [{value:"Cotabato", text:"Cotabato"},
+                      {value:"Cotabato City", text:"Cotabato City"},
+                      {value:"General Santos City", text:"General Santos City"},
+                      {value:"Sarangani", text:"Sarangani"},
+                      {value:"South Cotabato", text:"South Cotabato"},
+                      {value:"Sultan Kudarat", text:"Sultan Kudarat"}];
+
+regiondb["Region XIII"] = [{value:"Agusan Del Norte", text:"Agusan Del Norte"},
+                      {value:"Agusan Del Sur", text:"Agusan Del Sur"},
+                      {value:"Butuan City", text:"Butuan City"},
+                      {value:"Dinagat Islands", text:"Dinagat Islands"},
+                      {value:"Surigao Del Norte", text:"Surigao Del Norte"},
+                      {value:"Surigao Del Sur", text:"Surigao Del Sur"}];
+
+regiondb["CAR"] = [{value:"Abra", text:"Abra"},
+                      {value:"Apayao", text:"Apayao"},
+                      {value:"Baguio City", text:"Baguio City"},
+                      {value:"Benguet", text:"Benguet"},
+                      {value:"Ifugao", text:"Ifugao"},
+                      {value:"Kalinga", text:"Kalinga"},
+                      {value:"Mountain Province", text:"Mountain Province"}];
+
+regiondb["NCR"] = [{value:"Manila", text:"Manila"},
+                      {value:"Caloocan", text:"Caloocan"},
+                      {value:"Las Pinas", text:"Las Pinas"},
+                      {value:"Makati", text:"Makati"},
+                      {value:"Malabon", text:"Malabon"},
+                      {value:"Mandaluyong", text:"Mandaluyong"},
+                      {value:"Marikina", text:"Marikina"},
+                      {value:"Muntinlupa", text:"Muntinlupa"},
+                      {value:"Navotas", text:"Navotas"},
+                      {value:"Pasay", text:"Pasay"},
+                      {value:"Pasig", text:"Pasig"},
+                      {value:"Pateros", text:"Pateros"},
+                      {value:"Paranaque", text:"Paranaque"},
+                      {value:"Quezon", text:"Quezon"},
+                      {value:"San Juan", text:"San Juan"},
+                      {value:"Taguig", text:"Taguig"},
+                      {value:"Valenzuela", text:"Valenzuela"}];
+
+function setCategory(chooser) {
+    var newElem;
+    var where = (navigator.appName == "Microsoft Internet Explorer") ? -1 : null;
+    var provinceChooser = chooser.form.elements["province"];
+    while (provinceChooser.options.length) {
+        provinceChooser.remove(0);
+    }
+    var choice = chooser.options[chooser.selectedIndex].value;
+    var db = regiondb[choice];
+    newElem = document.createElement("option");
+    newElem.text = "Please select province";
+    newElem.value = "";
+    provinceChooser.add(newElem, where);
+    if (choice != "") {
+        for (var i = 0; i < db.length; i++) {
+            newElem = document.createElement("option");
+            newElem.text = db[i].text;
+            newElem.value = db[i].value;
+            provinceChooser.add(newElem, where);
+        }
+    }
+}
