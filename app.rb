db6947eef12f1e57166b0a139ca82ed87fc5d91f
Almer Almazan <almeralmazan@gmail.com>
2012-05-31 11:36:10 +0800

    finally my first version.. yay!

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 require 'haml'
 require File.expand_path('../model.rb', __FILE__)
 
+# home page
 get '/' do
   @title = 'Health Card Subscription'
   haml :index
@@ -10,43 +11,34 @@ end
 
 # create record
 post '/create' do
-  p = Person.new
-  p.name = params[:name]
-  p.gender = params[:gender]
-  p.age = params[:age]
-  p.region = params[:region]
-  p.province = params[:province]
-  p.nature = params[:nature]
-  p.email = params[:email]
-  p.cpno = params[:cpno]
+  p              = Person.new
+  p.name         = params[:name]
+  p.gender       = params[:gender]
+  p.age          = params[:age]
+  p.region       = params[:region]
+  p.province     = params[:province]
+  p.nature       = params[:nature]
+  p.email        = params[:email]
+  p.cpno         = params[:cpno]
   p.emp_hcp_ans1 = params[:emp_hcp_ans1]
   p.emp_hcp_ans2 = params[:emp_hcp_ans2]
-  p.third_answer = params[:third_answer]
-  p.fourth_answer = params[:fourth_answer]
-  p.fin_con_ans1 = params[:fin_con_ans1]
-  p.fin_con_ans2 = params[:fin_con_ans2]
-  p.comments = params[:comments]
+  p.ape          = params[:ape]
+  p.ipc          = params[:ipc]
+  p.opc          = params[:opc]
+  p.ec           = params[:ec]
+  p.dc           = params[:dc]
+  p.fca          = params[:fca]
+  p.others       = params[:others]
+  p.no_answer    = params[:no_answer]
+  p.fin_con_ans1 = params[:fin_ans1]
+  p.fin_con_ans2 = params[:fin_amount]
+  p.comments     = params[:comments]
 
   if p.save
     #status 201
-    redirect '/success'
+    haml :success
   else
     #status 412
     redirect '/failed'
   end
 end
-
-# test only
-post '/test' do
-  @p = Person.new
-  @p.name = params[:name]
-  @p.gender = params[:gender]
-  @p.age = params[:age]
-  @p.region = params[:region]
-  @p.province = params[:province]
-  @p.nature = params[:nature]
-  @p.email = params[:email]
-  @p.cpno = params[:cpno]
-  haml :list
-end
-
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,10 @@
 // manipulate other amount
 // from input text box
-function grabData() {
+function createInputTextBox() {
   var amountValue = document.forms[0].fin_amount.value;
 
   if (amountValue == "others") {
-    document.getElementById('others').innerHTML = "<input type='text' name='others' placeholder='Type amount here...'/>"; 
+    document.getElementById('others').innerHTML = "<input type='text' name='fin_amount' placeholder='Type amount here...'/>"; 
   } else {
     document.getElementById('others').innerHTML = ""; 
   }
