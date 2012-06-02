613783da5f0808e6fe0091fa38819b16455f15a9
Almer Almazan <almeralmazan@gmail.com>
2012-06-02 12:58:15 +0800

    finally contented and finished

--- app.rb
+++ app.rb
@@ -42,3 +42,31 @@ post '/create' do
     redirect '/failed'
   end
 end
+
+# test only
+post '/test' do
+  @p              = Person.new
+  @p.name         = params[:name]
+  @p.gender       = params[:gender]
+  @p.age          = params[:age]
+  @p.region       = params[:region]
+  @p.province     = params[:province]
+  @p.nature       = params[:nature]
+  @p.email        = params[:email]
+  @p.cpno         = params[:cpno]
+  @p.emp_hcp_ans1 = params[:emp_hcp_ans1]
+  @p.emp_hcp_ans2 = params[:emp_hcp_ans2]
+  @p.ape          = params[:ape]
+  @p.ipc          = params[:ipc]
+  @p.opc          = params[:opc]
+  @p.ec           = params[:ec]
+  @p.dc           = params[:dc]
+  @p.fca          = params[:fca]
+  @p.others       = params[:others]
+  @p.no_answer    = params[:no_answer]
+  @p.fin_con_ans1 = params[:fin_ans1]
+  @p.fin_con_ans2 = params[:fin_amount]
+  @p.comments     = params[:comments]
+
+  haml :test
+end
--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,26 @@
+$(document).ready( function() {
+  $('#no_answer').click( function() {
+    if ($(this).is(':checked')) {
+      $('#six-chbox-one-input-text').slideToggle('slow'); 
+      $('input[name=ape], input[name=ipc], input[name=opc], input[name=ec], input[name=dc], input[name=fca]').attr('checked', false);
+
+      $('input[name=others]').val('');
+
+    } else {
+      $('#six-chbox-one-input-text').slideToggle('slow'); 
+    }
+  })
+});
+
+
 // manipulate other amount
 // from input text box
 function createInputTextBox() {
   var amountValue = document.forms[0].fin_amount.value;
 
   if (amountValue == "others") {
-    document.getElementById('others').innerHTML = "<input type='text' name='fin_amount' placeholder='Type amount here...'/>"; 
+    document.getElementById('others').innerHTML = "<input type='text' name='fin_amount' placeholder='Type amount here...' style='margin-left: 15px;' />"; 
+  
   } else {
     document.getElementById('others').innerHTML = ""; 
   }
