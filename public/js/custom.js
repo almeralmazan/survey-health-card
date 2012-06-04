82b641452f92dadb362094042f0639287cda570c
Almer Almazan <almeralmazan@gmail.com>
2012-06-04 15:06:49 +0800

    added ajax load but not yet finished

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,7 +9,23 @@ $(document).ready( function() {
     } else {
       $('#six-chbox-one-input-text').slideToggle('slow'); 
     }
-  })
+  });
+
+  // assuming your form has id="theform"
+  // defines a callback on submission of the form #theform
+  $('#theform').submit( function() {
+    // posts the contents of the form to /create using ajax
+    $.post('/create', $('#theform').serialize(), function(result) {
+      // assuming result is a string of the updated data in html
+      // and assuming that your data goes in an element with the id main-content
+      $('#main-content').html(result)
+      //var conf = confirm('Successfully created');
+      //if (conf) window.location.reload(true); 
+    }); 
+    return false; // prevents the form from submitting normally
+  });  
+
+
 });
 
 
